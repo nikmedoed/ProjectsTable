701e2ad3017a197ada314bc736f14aecfbf12111
Nikita <grafniksan@gmail.com>
2023-11-28 12:13:41 +0400

    borders fixes

--- src/styles.js
+++ src/styles.js
@@ -3,21 +3,22 @@ const HEADER_BACK = "#D9D9D9"
 
 const DARK_GRAY = "#333333";
 
+
 function setBorder(range) {
-  var color = SOLID_HEADER_COLOR
-  range.setBorder(true, true, true, true, false, false, color, SpreadsheetApp.BorderStyle.SOLID);
+  range.setBorder(true, true, true, true, true, true, SOLID_HEADER_COLOR, SpreadsheetApp.BorderStyle.SOLID);
   return range
 }
 
 function setTopBorder(range) {
-  range.setBorder(true, false, false, false, false, false, SOLID_HEADER_COLOR, SpreadsheetApp.BorderStyle.SOLID_MEDIUM);
+  range.setBorder(true, null, null, null, null, null, SOLID_HEADER_COLOR, SpreadsheetApp.BorderStyle.SOLID_MEDIUM);
   return range;
 }
 
 function setBlockBorder(range) {
-  range.setBorder(true, true, false, true, false, false, SOLID_HEADER_COLOR, SpreadsheetApp.BorderStyle.SOLID_MEDIUM);
+  range.setBorder(true, true, null, true, null, null, SOLID_HEADER_COLOR, SpreadsheetApp.BorderStyle.SOLID_MEDIUM);
   return range;
 }
+
 
 function setRowStyleMain(range, leftAlignedCells = []) {
   range.setHorizontalAlignment('center');
@@ -39,4 +40,38 @@ function rotateAndAlign(range) {
 function setBackHeader(range) {
   range.setBackground(HEADER_BACK);
   return range
+}
+
+
+function fixBorders() {
+  let projectMap = SSheet.getSheetByName(TEMPLATE_MAP)
+  var mapValues = projectMap.getDataRange().getValues();
+  var lastRow = 0;
+  for (var i = mapValues.length - 1; i >= 0; i--) {
+    if (mapValues[i][0]) {
+      lastRow = i + 1;
+      let timeline = findTimelineBorders(projectMap)
+      fixBordersOnRange(projectMap.getRange(timeline.endRow + 1, 1, lastRow - timeline.endRow, projectMap.getLastColumn()), timeline)
+      break;
+    }
+  }
+
+  getBlockSheets().forEach(sheet => {
+    let = timeline = findTimelineBorders(sheet)
+    let range = sheet.getRange(ROW_START, 1, sheet.getLastRow() - ROW_START + 1, sheet.getLastColumn())
+    fixBordersOnRange(range, timeline)
+  })
+}
+
+function fixBordersOnRange(range, timeline) {
+  let sheet = range.getSheet()
+  setBorder(range)
+  if (!timeline) {
+    timeline = findTimelineBorders(sheet)
+  }
+  let colHei = range.getLastRow() + 1 - timeline.row
+  let merged = sheet.getRange(timeline.row, timeline.startCol, 1, timeline.col - timeline.startCol + 1).getMergedRanges()
+  merged.forEach(mer => { 
+    setBlockBorder(sheet.getRange(timeline.row,  mer.getColumn(), colHei, mer.getWidth()))  
+  })
 }
